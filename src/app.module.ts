c75275266ae0300d820c56937f95d7aedd75bd5e
Bruno12leonel <bruno12leonel@gmail.com>
2024-12-06 19:47:01 -0300

    Remove developer-related files and implement employee, task, meeting, and contact info entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DevelopersModule } from './developers/developers.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Employee } from './entities/employee.entity';
+import { ContactInfo } from './entities/contact-info.entity';
+import { Task } from './entities/task.entity';
+import { Meeting } from './entities/meeting.entity';
 
 @Module({
   imports: [
@@ -11,10 +13,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'db.sqlite',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      logging: true,
     }),
-    DevelopersModule,
+    TypeOrmModule.forFeature([Employee, ContactInfo, Task, Meeting]),
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { existsSync, unlinkSync } from 'fs';
+import { AppService } from './app.service';
 
 async function bootstrap() {
+  const dbFile = 'db.sqlite';
+  if (existsSync(dbFile)) unlinkSync(dbFile);
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
+
+  const appService = app.get(AppService);
+  await appService.seed();
   await app.listen(3000);
 }
 bootstrap();
